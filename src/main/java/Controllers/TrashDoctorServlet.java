aacdba7750dd7c49cf18790c7dc43195e1870c03
MOHAMMED ELACHYRY <116925209+elachyry@users.noreply.github.com>
2023-03-07 23:56:53 +0100

    Create TrashDoctorServlet.java

---
+++ src/main/java/Controllers/TrashDoctorServlet.java
@@ -0,0 +1,20 @@
+package Controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/TrachDoctor")
+
+public class TrashDoctorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/Admin/TrashDoctor.jsp").forward(request, response);
+	}
+
+
+}
